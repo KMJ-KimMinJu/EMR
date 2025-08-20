3eec0cbfc5a3e29159143020c2d784c540f470ba
KimMinJu <94983600+KMJ-KimMinJu@users.noreply.github.com>
2025-08-20 21:40:22 +0900

    Update next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      { source: "/api/:path*", destination: "http://127.0.0.1:4000/:path*" },
+      { source: "/api/:path*", destination: "http://127.0.0.1:4000/api/:path*" },
     ];
   },
 };
